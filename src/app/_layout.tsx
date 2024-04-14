31abf32161ca901e1818a47a37ca860ea048f58f
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-04-14 20:18:52 -0300

    inicio de projeto

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import '../styles/global.css'
+import { Slot } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+
+export default function Layout() {
+  return (
+    <>
+      <StatusBar style="light"/>
+      <Slot />
+    </>
+  )
+}
