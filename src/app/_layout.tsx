6231ec4ea1e0f32960138422283ed4cea7413078
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-04-14 21:37:50 -0300

    componentes, tela de home e register

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,28 @@
-import '../styles/global.css'
+import '@/styles/global.css'
+
 import { Slot } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
+
+import { 
+  useFonts, 
+  Roboto_700Bold,
+  Roboto_500Medium,
+  Roboto_400Regular
+} from '@expo-google-fonts/roboto'
+
+import { Loading } from '@/components/loading'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
-    <>
-      <StatusBar style="light"/>
-      <Slot />
-    </>
+    <Slot />  
   )
 }
--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,5 +1,5 @@
 import { View, Image, StatusBar } from 'react-native'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { FontAwesome6, MaterialIcons } from '@expo/vector-icons'
 
 import { Link } from 'expo-router'
 
@@ -8,7 +8,7 @@ import { colors } from '@/styles/color'
 import { Input } from '@/components/input'
 import { Button } from '@/components/button'
 
-export default function Home() {
+export default function Register() {
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content"/>
@@ -20,12 +20,21 @@ export default function Home() {
       
       <View className="w-full mt-12 gap-3">
         <Input>
-          <MaterialCommunityIcons 
-            name="ticket-confirmation-outline" 
+          <FontAwesome6 
+            name="user-circle" 
             color={colors.green[200]}
             size={20} 
           />
-          <Input.Field placeholder='Código do ingresso'/>
+          <Input.Field placeholder='Nome completo'/>
+        </Input>
+
+        <Input>
+          <MaterialIcons 
+            name="alternate-email" 
+            color={colors.green[200]}
+            size={20} 
+          />
+          <Input.Field placeholder='E-mail' keyboardType='email-address'/>
         </Input>
 
         <Button title="Acessar credencial"/>
